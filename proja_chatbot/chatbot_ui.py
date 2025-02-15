d2e6de9d7caf26fa97acaf61cd3479d6f9c826f4
jzphlp <josephluntadilap@gmail.com>
2025-02-15 03:02:51 +0000

    add chatboot with gradio and deepscaler

---
+++ proja_chatbot/chatbot_ui.py
@@ -0,0 +1,20 @@
+import gradio as gr
+import ollama
+
+# Function to interact with the Deepscaler chatbot
+def chat_with_bot(user_message):
+	#deepscaler::deepscaler-chat'
+	response = ollama.chat(model='deepscaler', messages=[{"role": "user", "content": user_message}])
+	return response["message"]
+
+# Define Geadio Chatbot Interface
+chatbot_ui = gr.Interface(
+	fn = chat_with_bot,
+	inputs=gr.Textbox(label="Chat Message"),
+	outputs=gr.Textbox(label="Bot Response"),
+	title="AI-Chatbot using DeepScaler",
+	description="Chat with an AI chatbot powered by DeepScaler and Ollama."
+)
+
+if __name__ == "__main__":
+	chatbot_ui.launch()
